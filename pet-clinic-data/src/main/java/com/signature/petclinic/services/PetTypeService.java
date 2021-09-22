df8237bc8727f62c2c5531180a4a48d0620e6622
jarvis <atul.atendra15@gmail.com>
2021-09-22 16:00:00 +0530

    added PetTypeService. closes #30

---
+++ pet-clinic-data/src/main/java/com/signature/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.signature.petclinic.services;
+
+import com.signature.petclinic.model.PetType;
+
+public interface PetTypeService extends CurdService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/signature/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.signature.petclinic.services.map;
+
+import com.signature.petclinic.model.PetType;
+import com.signature.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+}
