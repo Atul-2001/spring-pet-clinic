ffd3c938f53cd34a862f9c703ec5ef40ad1869c1
jarvis <atul.atendra15@gmail.com>
2021-09-24 11:38:18 +0530

    added Visit Entity. closes #39

--- pet-clinic-data/src/main/java/com/signature/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/signature/petclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.signature.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +22,13 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthdate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits;
+
+    public Pet() {
+        this.visits = new HashSet<>();
+    }
 
     public String getName() {
         return name;
@@ -53,4 +62,11 @@ public class Pet extends BaseEntity {
         this.birthdate = birthdate;
     }
 
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 }
