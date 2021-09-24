8414f7a7c5eb6a7290f199f095fd1aaa1e062030
jarvis <atul.atendra15@gmail.com>
2021-09-24 16:00:06 +0530

    added spring data jpa service for Visit. closes #48

---
+++ pet-clinic-data/src/main/java/com/signature/petclinic/services/jpa/VisitJpaService.java
@@ -0,0 +1,48 @@
+package com.signature.petclinic.services.jpa;
+
+import com.signature.petclinic.model.Visit;
+import com.signature.petclinic.repository.VisitRepository;
+import com.signature.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+@Profile("spring-data-jap")
+public class VisitJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Visit save(Visit entity) {
+        return visitRepository.save(entity);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return StreamSupport.stream(visitRepository.findAll().spliterator(), false)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit entity) {
+        visitRepository.delete(entity);
+    }
+}
