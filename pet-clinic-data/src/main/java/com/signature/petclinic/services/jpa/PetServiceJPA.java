899d49540ddf79bd63103423bfdfae4d3eb81337
jarvis <atul.atendra15@gmail.com>
2021-09-24 14:54:38 +0530

    added spring data jpa service for Pet. closes #43

---
+++ pet-clinic-data/src/main/java/com/signature/petclinic/services/jpa/PetServiceJPA.java
@@ -0,0 +1,48 @@
+package com.signature.petclinic.services.jpa;
+
+import com.signature.petclinic.model.Pet;
+import com.signature.petclinic.repository.PetRepository;
+import com.signature.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+@Profile("spring-data-jpa")
+public class PetServiceJPA implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetServiceJPA(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Pet save(Pet entity) {
+        return petRepository.save(entity);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return StreamSupport.stream(petRepository.findAll().spliterator(), false)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet entity) {
+        petRepository.delete(entity);
+    }
+}
