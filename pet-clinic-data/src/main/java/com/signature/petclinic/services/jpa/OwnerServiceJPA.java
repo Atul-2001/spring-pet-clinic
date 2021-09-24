d46e89dfd5f5727441b7f347d6379063a1a7a07c
jarvis <atul.atendra15@gmail.com>
2021-09-24 12:23:11 +0530

    added spring data jpa service for Owner. closes #41

---
+++ pet-clinic-data/src/main/java/com/signature/petclinic/services/jpa/OwnerServiceJPA.java
@@ -0,0 +1,53 @@
+package com.signature.petclinic.services.jpa;
+
+import com.signature.petclinic.model.Owner;
+import com.signature.petclinic.repository.OwnerRepository;
+import com.signature.petclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+@Profile("spring-data-jpa")
+public class OwnerServiceJPA implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+
+    public OwnerServiceJPA(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Owner save(Owner entity) {
+        return ownerRepository.save(entity);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return ownerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        return StreamSupport.stream(ownerRepository.findAll().spliterator(), false)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner entity) {
+        ownerRepository.delete(entity);
+    }
+}
