adfed21f1f39921a2fb3f4ea53d2f85a34dc2a96
Atendra Suryvanshi <suryavanshi.atendra15@gmail.com>
2022-05-09 12:53:56 +0530

    adding vets json endpoint. closes #64

--- pet-clinic-web/src/main/java/com/signature/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/signature/petclinic/controller/VetController.java
@@ -1,14 +1,16 @@
 package com.signature.petclinic.controller;
 
+import com.signature.petclinic.model.Vet;
 import com.signature.petclinic.services.VetService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
-@RequestMapping("/vets")
 public class VetController {
 
   private final VetService vetService;
@@ -17,10 +19,14 @@ public class VetController {
     this.vetService = vetService;
   }
 
-  @GetMapping({"", "/", "/index", "/index.html"})
+  @GetMapping({"/vets", "/vets/", "/vets/index", "/vets/index.html"})
   public String index(Model model, Pageable pageable) {
     model.addAttribute("searchResult", vetService.findAll(pageable));
     return "vets/index";
   }
 
+  @GetMapping("/api/vets")
+  public @ResponseBody Set<Vet> getVetsJson() {
+    return vetService.findAll();
+  }
 }
